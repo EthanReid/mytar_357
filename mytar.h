f1d4529845fa819696c87ffc24a39337d8d14bea
hamzaaziz <haziz@calpoly.edu>
2023-03-06 10:41:55 -0800

    Test Commit #3? 3/6

---
+++ mytar.h
@@ -0,0 +1,36 @@
+#ifndef MYTAR_H
+#define MYTAR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+struct header {
+    char name[100];
+    char mode[8];
+    char uid[8];
+    char gid[8];
+    char size[12];
+    char mtime[12];
+    char chksum[8];
+    char typeflag;
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+    char padding[12];	
+};
+
+
+
+#endif
