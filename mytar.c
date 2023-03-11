90c084ac97ff66c75783479095e47ce540ed7680
hmaziz <102627606+hmaziz@users.noreply.github.com>
2023-03-10 22:40:22 -0800

    Knitting Ethan's code with my own

--- mytar.c
+++ mytar.c
@@ -1,7 +1,16 @@
 #include "mytar.h"
-/*This file contains various helper methods that are used 
-across all files*/
+FILE *out_file;
 
-int main(int argc, char*argv[]){
-    
+int main(int argc, char **argv){
+    //if file already exists it needs to be wiped
+    //open file in w mode first, write nothing, close, reopen in a mode
+    out_file = fopen("testout.tar","w");
+    close(out_file);
+    out_file = fopen("testout.tar","a");
+    manage_file(argv[1]);
+    write_block(FORCE);
+    write_block(FORCE);
 }
+
+char mem_block[512];
+
--- mytar.h
+++ mytar.h
@@ -1,6 +1,3 @@
-#ifndef MYTAR_H
-#define MYTAR_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,26 +7,32 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include "archive.h"
+#include "blockBuffer.h"
 
-typedef struct Header {
-    char name[100];
-    char mode[8];
-    char uid[8];
-    char gid[8];
-    char size[12];
-    char mtime[12];
-    char chksum[8];
-    char typeflag;
-    char linkname[100];
-    char magic[6];
-    char version[2];
-    char uname[32];
-    char gname[32];
-    char devmajor[8];
-    char devminor[8];
-    char prefix[155];
-    char padding[12];	
-} Header;
+extern FILE *out_file;
+#ifndef MYTAR_H
+#define MYTAR_H
+
+// typedef struct Header {
+//     char name[100];
+//     char mode[8];
+//     char uid[8];
+//     char gid[8];
+//     char size[12];
+//     char mtime[12];
+//     char chksum[8];
+//     char typeflag;
+//     char linkname[100];
+//     char magic[6];
+//     char version[2];
+//     char uname[32];
+//     char gname[32];
+//     char devmajor[8];
+//     char devminor[8];
+//     char prefix[155];
+//     char padding[12];	
+// } Header;
 
 
 
