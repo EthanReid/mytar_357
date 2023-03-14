b2ff42bccd8af55e27dba46d2fb49cd0ef9a8daf
Ethan Reid <erpreid@gmail.com>
2023-03-14 15:54:07 -0700

    Update mytar.c

--- mytar.c
+++ mytar.c
@@ -17,14 +17,14 @@ void read_head(FILE *infile){
 int main(int argc, char **argv){
 //     //if file already exists it needs to be wiped
 //     //open file in w mode first, write nothing, close, reopen in a mode
-/* 
+
     out_file = fopen("testout.tar","w");
     close(out_file);
     out_file = fopen("testout.tar","a");
     manage_file(argv[1]);
     write_block(FORCE);
     write_block(FORCE);
-*/
+
 /*
     FILE *infile = fopen(argv[1], "rb");
     read_head(infile);
