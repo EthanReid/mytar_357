e3a47d177aa1e2a2ca97e4fdc9911b4866c460c4
Ethan Reid <erpreid@gmail.com>
2023-03-14 16:16:36 -0700

    Update mytar.c

--- mytar.c
+++ mytar.c
@@ -18,17 +18,17 @@ int main(int argc, char **argv){
 //     //if file already exists it needs to be wiped
 //     //open file in w mode first, write nothing, close, reopen in a mode
 
-    out_file = fopen("testout.tar","w");
-    close(out_file);
-    out_file = fopen("testout.tar","a");
-    manage_file(argv[1]);
-    write_block(FORCE);
-    write_block(FORCE);
-
-/*
-    FILE *infile = fopen(argv[1], "rb");
-    read_head(infile);
-*/
+    if (strcmp(argv[1], "-c")==0){
+        out_file = fopen("testout.tar","w");
+        close(out_file);
+        out_file = fopen("testout.tar","a");
+        manage_file(argv[2]);
+        write_block(FORCE);
+        write_block(FORCE);
+    }else{
+        FILE *infile = fopen(argv[2], "rb");
+        read_head(infile);
+    }
 
 }
 
